514a04ef01d76f1d16889a279534831bcd952cc0
Gagik <gagikpog@gmail.com>
2019-03-17 00:16:14 +0500

    change readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Tetris
+### Using libs
+- ##### MinGW
+- ##### OpenGl
+- ##### OpenAL
+
+#### install libs
+```sh
+$ chmod +x installLibs.sh
+$ ./installLibs.sh
+```
+Compile with `makefile` or
+```sh
+$ ./makeAndRun.sh
+$ ./makeDebug.sh
+```
---
+++ installLibs.sh
@@ -0,0 +1,7 @@
+sudo apt-get install libalut-dev
+sudo apt-get install libsdl-sound1.2-dev
+#OpenAL compile keys -lopenal -lalut
+sudo apt install g++
+sudo apt install make
+sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
+#OpenGl compile keys -lglut -lGLU -lGL
